8a3f8fa4c4a8d3eff8ea9623b602ad438476dfd3
roshni090496 <rs09041001@gmail.com>
2022-07-13 01:43:14 -0700

    Added : Curd Operation

--- src/main/java/com/example/day4assignspringemployeepayroll/service/iEmployeeService.java
+++ src/main/java/com/example/day4assignspringemployeepayroll/service/iEmployeeService.java
@@ -1,6 +1,6 @@
 package com.example.day4assignspringemployeepayroll.service;
 
-//import com.example.day4assignspringemployeepayroll.model.Employee;
+import com.example.day4assignspringemployeepayroll.model.Employee;
 
 import java.util.List;
 
@@ -8,14 +8,14 @@ public interface iEmployeeService {
     String EmployeeMessage();
 
     //    *****UC2*****
-//    Employee findEmployeeById(Integer id);
-//
-//    Employee saveEmployees(Employee employee);
-//
-//    List<Employee> findEmployees();
-//
-//    Employee editEmployee(Employee employee, Integer id);
-//
-//    String deleteEmployee(Integer id);
+    Employee findEmployeeById(Integer id);
+
+    Employee saveEmployees(Employee employee);
+
+    List<Employee> findEmployees();
+
+    Employee editEmployee(Employee employee, Integer id);
+
+    String deleteEmployee(Integer id);
 
  }
